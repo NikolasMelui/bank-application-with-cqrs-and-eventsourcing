28428ba2213643c07a7645271606ebb083e06bec
nikolasmelui <slak@samaradom.ru>
2021-03-30 22:13:23 +0300

    Start to create business logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 'use strict';
-const server = require('./server');
 
-const { APPLICATION_HOST, APPLICATION_PORT } = require('./config');
-
-const host = process.env.APPLICATION_HOST || APPLICATION_HOST;
-const port = process.env.APPLICATION_PORT || APPLICATION_PORT;
+const { EventEmitter } = require('events');
+const eventBus = new EventEmitter();
 
 (async () => {
-  await server(host, port);
+  console.log(eventBus);
 })();
